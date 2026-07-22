ca59893a855640e86193e8cd3da001197650267a
jajodiaz <jajodiaz@espol.edu.ec>
2026-07-22 14:43:35 -0500

    TESTCALCULARPRECIO
    Se realizaron los test para cada caso del switch en el metodo calcular precio base, incluyendo el manejo de la excepcion de un tipo= null

--- taller-junit-cine/src/test/java/AsientoTest.java
+++ taller-junit-cine/src/test/java/AsientoTest.java
@@ -17,4 +17,38 @@ class AsientoTest {
     }
 
     // TODO: Traducir los demás casos de su tabla a métodos @Test.
+
+    @Test
+    @DisplayName("calculando precio base ESTANDAR")
+    void CalPrecioESTANDAR(){
+        asiento = new Asiento("A3", "ESTANDAR");
+       assertEquals(5.0, asiento.calcularPrecioBase());
+    }
+    @Test
+    @DisplayName("Calculando precio base VIP")
+    void CalPrecioVIP(){
+        asiento = new Asiento("A2", "VIP");
+       assertEquals(8.5, asiento.calcularPrecioBase());
+    }
+    @Test
+    @DisplayName("Calculando precio base 4D")
+     void CalPrecio4D(){
+        asiento = new Asiento("A4", "4D");
+       assertEquals(12.0, asiento.calcularPrecioBase());
+    }
+    @Test
+    @DisplayName("Calculando precio base DEFAULT")
+     void CalPrecioDefault(){
+        asiento = new Asiento("A4", "OTRO VALOR");
+       assertEquals(0.0, asiento.calcularPrecioBase());
+    }
+    @Test
+    @DisplayName("ERROR DE TIPO NULL")
+     void CalPrecioNull(){
+        asiento = new Asiento("A4", null);
+       assertThrows(NullPointerException.class, () -> {
+        asiento.calcularPrecioBase();
+    });
+
+    }
 }
